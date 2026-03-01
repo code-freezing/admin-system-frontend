f4da3dfdb5810fde1b121929d16acbd10a3c5161
codefreezing <3102637943@qq.com>
2026-03-01 22:47:26 +0800

    初始化项目并配置相关环境

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <el-button type="primary">Hello Element Plus</el-button>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`
+      }
+    }
+  },
+  server: {
+    port: 8080,
+    open: true,
+    cors: true,
+  }
+})
