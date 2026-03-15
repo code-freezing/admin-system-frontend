cdd8ad050e38ea8daf40630897f7db8250daa5f9
codefreezing <3102637943@qq.com>
2026-03-15 19:53:27 +0800

    完成了产品管理的模块

--- src/router/index.ts
+++ src/router/index.ts
@@ -52,16 +52,16 @@ const router = createRouter({
           path: '/users_manage',
           component: () => import('@/views/user_manage/users_manage/index.vue'),
         },
-        // {
-        //   name: 'product_manage_list',
-        //   path: '/product_manage_list',
-        //   component: () => import('@/views/product/product_manage_list/index.vue'),
-        // },
-        // {
-        //   name: 'out_product_manage_list',
-        //   path: '/out_product_manage_list',
-        //   component: () => import('@/views/product/out_product_manage_list/index.vue'),
-        // },
+        {
+          name: 'product_manage_list',
+          path: '/product_manage_list',
+          component: () => import('@/views/product/product_manage_list/index.vue'),
+        },
+        {
+          name: 'out_product_manage_list',
+          path: '/out_product_manage_list',
+          component: () => import('@/views/product/out_product_manage_list/index.vue'),
+        },
         // {
         //   name: 'message_list',
         //   path: '/message_list',
